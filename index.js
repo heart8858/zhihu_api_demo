90434077c258c7decf1d19d71f3bad27d8112188
chen <heart8858@gmail.com>
2020-12-04 23:47:59 +0800

    second commit

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const Koa = require('koa');
 const app = new Koa();
+const Router = require('koa-router');
+const router = new Router();
+const userRouter = new Router({ prefix: '/users' });
 
 
-app.use(async (ctx) => {
-    if (ctx.url === '/') {
-        ctx.body = '这是主页';
-    } else if (ctx.url === '/users') {
-        if (ctx.method === 'GET') {
-            ctx.body = '这是用户列表页';
-        } else if (ctx.method === 'POST') {
-            ctx.body = '创建用户';
-        } else {
-            ctx.status = 405;
-        }
-    } else if (ctx.url.match(/\/users\/\w+/)) {
-        const userId = ctx.url.match(/\/users\/(\w+)/)[1];
-        ctx.body = `这是用户 ${userId}`;
-    }
-    else {
-        ctx.status = 404;
-    }
-})
+
+router.get('/', (ctx) => {
+    ctx.body = '这是主页'
+});
+
+userRouter.get('/', (ctx) => {
+    ctx.body = [{ name: '李雷' }, { name: '韩梅梅' }];
+});
+
+userRouter.post('/', (ctx) => {
+    ctx.body = { name: '李雷' };
+});
+
+userRouter.get('/:id', (ctx) => {
+    ctx.body = { name: '李雷' };
+});
+
+userRouter.put('/:id', (ctx) => {
+    ctx.body = { name: '李雷2' };
+});
+
+userRouter.delete('/:id', (ctx) => {
+    ctx.status = 204;
+});
+
+
+app.use(router.routes());
+app.use(userRouter.routes());
+app.use(userRouter.allowedMethods());
 
 app.listen(3000);
